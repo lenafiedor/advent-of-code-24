d76f6ed297b85f4ad017a78a36c6f8a0b9f4c05f
lenafiedor <lenafiedor@o2.pl>
2024-12-05 19:01:29 +0100

    Second start & restructure

--- day-05/src/main/java/SecondStar.java
+++ day-05/src/main/java/SecondStar.java
@@ -1,26 +1,43 @@
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class SecondStar {
 
     public static final String FILE_NAME = "printing_rules.txt";
 
-    private static boolean isRuleValid(List<Integer> rule, List<Integer> list) {
-        return list.indexOf(rule.get(0)) < list.indexOf(rule.get(1));
+    private static boolean isRuleInvalid(List<Integer> rule, List<Integer> list) {
+        return list.indexOf(rule.get(0)) > list.indexOf(rule.get(1));
     }
 
     private static void swap(List<Integer> list, List<Integer> rule) {
-        int index1 = rule.get(0);
-        int index2 = rule.get(1);
 
-        int temp = list.get(index1);
-        list.set(index1, list.get(index2));
-        list.set(index2, temp);
+        int first = rule.get(0);
+        int second = rule.get(1);
+
+        Collections.swap(list, list.indexOf(first), list.indexOf(second));
+    }
+
+    private static boolean isListInvalid(List<Integer> list, List<List<Integer>> rules) {
+
+        for (List<Integer> rule : rules) {
+            if (new HashSet<>(list).containsAll(rule) && isRuleInvalid(rule, list)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static void updateList(List<Integer> list, List<List<Integer>> rules) {
+
+        while (isListInvalid(list, rules)) {
+            for (List<Integer> rule : rules) {
+                if (new HashSet<>(list).containsAll(rule) && isRuleInvalid(rule, list)) {
+                    swap(list, rule);
+                }
+            }
+        }
     }
 
     private static List<List<Integer>> validateList(
@@ -29,14 +46,8 @@ public class SecondStar {
         List<List<Integer>> validList = new ArrayList<>();
 
         for (List<Integer> list : printList) {
-            boolean wasInvalid = false;
-            for (List<Integer> rule : rules) {
-                if (list.containsAll(rule) && !isRuleValid(rule, list)) {
-                    wasInvalid = true;
-                    swap(list, rule);
-                }
-            }
-            if (wasInvalid) {
+            if (isListInvalid(list, rules)) {
+                updateList(list, rules);
                 validList.add(list);
             }
         }
@@ -49,10 +60,23 @@ public class SecondStar {
                 .sum();
     }
 
+    private static List<List<Integer>> parseInput(Scanner scanner, String regex) {
+
+        List<List<Integer>> input = new ArrayList<>();
+
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            if (line.isEmpty()) break;
+            List<Integer> numbers = Arrays.stream(line.split((regex)))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+            input.add(numbers);
+        }
+        return input;
+    }
+
     public static void main (String[] args) {
 
-        List<List<Integer>> rules = new ArrayList<>();
-        List<List<Integer>> printList = new ArrayList<>();
         ClassLoader classLoader = SecondStar.class.getClassLoader();
 
         try (InputStream inputStream = classLoader.getResourceAsStream(FILE_NAME)) {
@@ -60,27 +84,16 @@ public class SecondStar {
                 return;
             }
             Scanner scanner = new Scanner(inputStream);
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                if (line.isEmpty()) { break; }
-                List<Integer> numbers = Arrays.stream(line.split(("\\|")))
-                        .map(Integer::parseInt)
-                        .collect(Collectors.toList());
-                rules.add(numbers);
-            }
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                List<Integer> numbers = Arrays.stream(line.split((",")))
-                        .map(Integer::parseInt)
-                        .collect(Collectors.toList());
-                printList.add(numbers);
-            }
+
+            List<List<Integer>> rules = parseInput(scanner, "\\|");
+            List<List<Integer>> printList = parseInput(scanner, ",");
+
+            int sum = calculateSum(validateList(rules, printList));
+            System.out.println("Sum: " + sum);
+
         } catch (IOException e) {
             throw new RuntimeException();
         }
-        List<List<Integer>> validList = validateList(rules, printList);
-        int sum = calculateSum(validList);
-        System.out.println(sum);
     }
 
 }
